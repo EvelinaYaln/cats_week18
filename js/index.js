e78e388d325f488dbe616be07a77e1eaa32e6e15
EvelinaYaln <evelina.dranets@gmail.com>
2022-02-26 22:15:26 +0300

    create class Cat, createObj()

---
+++ js/index.js
@@ -0,0 +1,45 @@
+'use strict'
+class Cat {
+    constructor(catsName, ownersName, contacts, breed, feed, gender, comment) {
+        this.catsName = catsName;
+        this.ownersName = ownersName;
+        this.contacts = contacts;
+        this.breed = breed;
+        this.feed = feed;
+        this.gender = gender;
+        this.comment = comment;
+    }
+}
+
+const createObj = () => {
+    const catsName = document.querySelector('#catsName').value;
+    const ownersName = document.querySelector('#ownersName').value;
+    const contacts = document.querySelector('#contacts').value;
+    const comment = document.querySelector('#comment').value;
+    const gender = document.querySelectorAll('[name="gender"]');
+    const breed = document.querySelector('#breed').value;
+    const feedDr = document.querySelector('#dryFeed');
+    const feedWet = document.querySelector('#wetFeed');
+    const feedMixed = document.querySelector('#mixFeed');
+    let gend = '';
+    for (let g of gender) {
+        if (g.checked) {
+            gend = g.value;
+        }
+    }
+    const arr = [];
+    if (feedDr.checked) {
+        arr.push(feedDr.value);
+    }
+    if (feedWet.checked) {
+        arr.push(feedWet.value);
+    }
+    if (feedMixed.checked) {
+        arr.push(feedMixed.value);
+    }
+
+    const cat = new Cat(catsName, ownersName, contacts, breed, arr, gend, comment);
+    console.log(cat);
+}
+
+save.addEventListener('click', createObj);
